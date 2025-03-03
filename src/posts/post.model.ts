9e9464edb34931e3791a7a53a23c75ec1567dcc8
peruibeloko <cmendonca32@gmail.com>
2025-03-03 12:59:19 -0300

    update post logic
    - use builder
    - finish update logic
    - implement iteration protocols

--- src/posts/post.model.ts
+++ src/posts/post.model.ts
@@ -3,6 +3,10 @@ import { db } from '@/config/config.service.ts';
 import { schema } from '@/persistence/sqlite.model.ts';
 import { Expr } from '@dldc/zendb';
 
+function filenameFromTitle(title: string) {
+  return title.toLowerCase().replaceAll(/\b\s+\b/g, '_');
+}
+
 export type Post = {
   /**
    * UUIDv4
@@ -28,53 +32,158 @@ export type Post = {
   modified_at: number;
 };
 
-function filenameFromTitle(title: string) {
-  return title.toLowerCase().replaceAll(/\b\s+\b/g, '_');
-}
+export class PostBuilder {
+  #id: null | string = null;
+  #title: null | string = null;
+  #author: null | string = null;
+  #path: null | string = null;
+  #content: null | string = null;
+  #draft: null | boolean = null;
+  #created_at: null | number = null;
+  #modified_at: null | number = null;
 
-// TODO Convert to builder
+  getContent() {
+    return this.#content ?? '';
+  }
+
+  id(v: string) {
+    this.#id = v;
+    return this;
+  }
+
+  title(v: string) {
+    this.#title = v;
+    return this;
+  }
+
+  author(v: string) {
+    this.#author = v;
+    return this;
+  }
+
+  path(v: string) {
+    this.#path = v;
+    return this;
+  }
+
+  content(v: string) {
+    this.#content = v;
+    return this;
+  }
+
+  draft(v: boolean) {
+    this.#draft = v;
+    return this;
+  }
+
+  created_at(v: number) {
+    this.#created_at = v;
+    return this;
+  }
+
+  modified_at(v: number) {
+    this.#modified_at = v;
+    return this;
+  }
+
+  build() {
+    return new PostEntity(this);
+  }
+
+  *[Symbol.iterator]() {
+    if (this.#id !== null) yield ['id', this.#id];
+    if (this.#title !== null) yield ['title', this.#title];
+    if (this.#author !== null) yield ['author', this.#author];
+    if (this.#path !== null) yield ['path', this.#path];
+    if (this.#draft !== null) yield ['draft', this.#draft];
+    if (this.#created_at !== null) yield ['created_at', this.#created_at];
+    if (this.#modified_at !== null) yield ['modified_at', this.#modified_at];
+  }
+
+  fromPost(postData: Post, content?: string) {
+    this.#id = postData.id;
+    this.#title = postData.title;
+    this.#author = postData.author;
+    this.#path = postData.path;
+    this.#draft = postData.draft;
+    this.#created_at = postData.created_at;
+    this.#modified_at = postData.modified_at;
+    if (content) this.#content = content;
+    return this;
+  }
+
+  toPost(): Post {
+    return {
+      id: this.#id ?? '',
+      title: this.#title ?? '',
+      author: this.#author ?? '',
+      path: this.#path ?? '',
+      draft: this.#draft ?? true,
+      created_at: this.#created_at ?? 0,
+      modified_at: this.#modified_at ?? 0,
+    };
+  }
+
+  toObject(): Partial<Post> {
+    return Object.fromEntries(this);
+  }
+
+  static NewPost(title: string, author: string, content: string) {
+    return new PostBuilder()
+      .id(crypto.randomUUID())
+      .title(title)
+      .author(author)
+      .path(
+        join(
+          normalize(Deno.cwd()),
+          'content',
+          `${filenameFromTitle(title)}.md`,
+        ),
+      )
+      .draft(true)
+      .created_at(Temporal.Now.instant().epochMilliseconds)
+      .modified_at(Temporal.Now.instant().epochMilliseconds)
+      .content(content)
+      .build();
+  }
+}
 
 export class PostEntity {
   #post: Post;
   #content: string;
 
-  private constructor(post: Post, content: string) {
-    this.#post = post;
-    this.#content = content;
+  constructor(builder: PostBuilder) {
+    this.#post = builder.toPost();
+    this.#content = builder.getContent();
   }
 
-  static new(title: string, author: string, content: string) {
-    return new PostEntity(
-      {
-        id: crypto.randomUUID(),
-        title,
-        author,
-        path: join(
-          normalize(Deno.cwd()),
-          'content',
-          `${filenameFromTitle(title)}.md`,
-        ),
-        draft: true,
-        created_at: Temporal.Now.instant().epochMilliseconds,
-        modified_at: Temporal.Now.instant().epochMilliseconds,
-      },
-      content,
+  #toPost(): Post {
+    return {
+      id: this.#post.id,
+      title: this.#post.title,
+      author: this.#post.author,
+      path: this.#post.path,
+      draft: this.#post.draft,
+      created_at: this.#post.created_at,
+      modified_at: this.#post.modified_at,
+    };
+  }
+
+  #getPath() {
+    const { path } = db.exec(
+      schema.posts
+        .query()
+        .select((c) => ({ path: c.path }))
+        .where((c) => Expr.equal(c.id, Expr.literal(this.#post.id)))
+        .first(),
     );
+
+    return path;
   }
 
   create() {
     Deno.writeTextFileSync(this.#post.path, this.#content);
-    db.exec(
-      schema.posts.insert({
-        id: this.#post.id,
-        title: this.#post.title,
-        author: this.#post.author,
-        path: this.#post.path,
-        draft: this.#post.draft,
-        created_at: this.#post.created_at,
-        modified_at: this.#post.modified_at,
-      }),
-    );
+    db.exec(schema.posts.insert(this.#toPost()));
   }
 
   static read(id: string) {
@@ -85,7 +194,7 @@ export class PostEntity {
         .first(),
     );
     const content = Deno.readTextFileSync(postData.path);
-    return new PostEntity(postData, content);
+    return new PostBuilder().fromPost(postData).content(content).build();
   }
 
   static list(page: number, size: number) {
@@ -98,10 +207,18 @@ export class PostEntity {
         .all(),
     );
 
-    return posts.map((post) => new PostEntity(post, ''));
+    return posts.map((postData) =>
+      new PostBuilder().fromPost(postData).build(),
+    );
   }
 
-  update() {}
+  update(id: string, builder: PostBuilder) {
+    const data = Object.fromEntries(builder) as Partial<Post>;
+    db.exec(schema.posts.updateEqual(data, { id }));
+
+    if (!builder.getContent()) return;
+    Deno.writeTextFileSync(this.#getPath(), builder.getContent());
+  }
 
   static delete(id: string) {
     const { path } = db.exec(
